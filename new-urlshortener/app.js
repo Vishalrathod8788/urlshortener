de079b7e0b9bda3f8ed28765f0e800ec9ebc81c1
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-05-17 17:18:39 +0530

    Refactor loadLink function for improved error handling and add shortcode validation in post request

--- new-urlshortener/app.js
+++ new-urlshortener/app.js
@@ -1,6 +1,7 @@
 import express from "express";
-import {readFile, writeFile} from "fs";
+import fs from "fs";
 import path from "path";
+import crypto from "crypto";
 
 const PORT = 5000;
 const DATA_FILE = path.join("data", "links.json");
@@ -12,31 +13,15 @@ app.use(express.static("public"));
 
 const loadLink = async () => {
     try {
-        await fs.mkdir("data", {recursive: true});
-
-        try {
-            const data = await fs.readFile(DATA_FILE, "utf-8");
-            // Check if data is empty
-            if(!data.trime()){
-                fs.writeFile(DATA_FILE, JSON.stringify({}));
-                return {};
-            }
-            return JSON.parse(data);
-        } catch (error) {
-            if (error.code === "ENOENT") {
-                // If file doesn't exist, create it with empty object
-                await fs.writeFile(DATA_FILE, JSON.stringify({}));
-                return {};
-            }
-            // If JSON parse error, reinitialize the file
-            if (error instanceof SyntaxError) {
-                await fs.writeFile(DATA_FILE, JSON.stringify({}));
-                return {};
-            }
-            throw error;
-        }
+        const data = fs.readFile(DATA_FILE, "utf-8");
+        return JSON.parse(data);
     } catch (error) {
-        
+        if(error.code === "ENOENT") {
+            // If file doesn't exist, create it with empty object
+            await fs.writeFile(DATA_FILE, JSON.stringify({}));
+            return {};
+        }
+        throw error;
     }
 }
 
@@ -52,10 +37,28 @@ app.get("/", (req, res) => {
     try {
         const file = fs.readFile(path.join("views", "index.html"));
         const links = loadLink();
+        
     } catch (error) {
         return res.status(500).send("Error loading index.html");
     }
 });
+
+app.post("/", (req, res) => {
+    try {
+        const {url, Shortcode} = req.body;
+        const finalShortCode = Shortcode || crypto.randomBytes(4).toString("hex");
+
+        const links = loadLink();
+
+        if(links[finalShortCode]){
+            return res.status(400).send("Shortcode already exists");
+        }
+        links[finalShortCode] = url;
+    } catch (error) {
+        console.error("Error saving link:", error);
+        return res.status(500).send("Error saving link");
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
